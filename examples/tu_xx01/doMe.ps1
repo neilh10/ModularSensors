676b79d813e4cfe9390f551a682e3336063cc22f
neilh20.gitk <neilh20+gitk1903@wLLw.net>
2021-05-22 09:55:22 -0700

    Add capture of zip on building

--- examples/tu_xx01/doMe.ps1
+++ examples/tu_xx01/doMe.ps1
@@ -6,4 +6,13 @@ $dest_dir = "..\..\..\releases"
 
 .\doBuild.ps1 $parm1  | Out-File -FilePath $log_file -NoClobber
 
+#If(Test-path $dest_logfile) {Remove-item $dest_logfile}
 Move-Item $log_file  $dest_dir
+
+Write-Output  "Capturing this unique libdeps"
+$source = ".pio\libdeps"
+$dest_zip = -join("maylfy_",$parm1,"_$parm2","_libdeps.zip"  )
+Add-Type -assembly "system.io.compression.filesystem"
+If(Test-path $dest_zip) {Remove-item $dest_zip}
+[io.compression.zipfile]::CreateFromDirectory($Source, $dest_zip) 
+Move-Item $dest_zip  $dest_dir
